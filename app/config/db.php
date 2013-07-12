ce42ff89d6858ba26fb51481378f38d0b49a30ac
Ilya Antipenko <aivus@aivus.name>
2013-07-12 23:55:13 +0300

    Extract db config from config/main.php to config/db.php Default dbname, dbuser, dbpassword: binary-network

---
+++ app/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'class'    => '\yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=binary-network',
+    'username' => 'binary-network',
+    'password' => 'binary-network',
+    'charset'  => 'utf8'
+);
--- app/config/main.php
+++ app/config/main.php
@@ -36,13 +36,7 @@ return array(
             )
         ),
 
-        'db' => array(
-            'class'    => '\yii\db\Connection',
-            'dsn'      => 'mysql:host=localhost;dbname=yii2',
-            'username' => 'root',
-            'password' => 'root',
-            'charset'  => 'utf8'
-        )
+        'db' => require 'db.php'
     ),
 
     'params' => $params
