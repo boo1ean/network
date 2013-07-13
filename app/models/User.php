1ea269b0c0fb13109c9feaa023331c95d3305e65
s0ph1e <sonya1912@gmail.com>
2013-07-13 18:48:04 +0300

    Add rules, scenarios, user addition in user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,29 +1,51 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Sophie
- * Date: 12.07.13
- * Time: 23:38
- * To change this template use File | Settings | File Templates.
- */
+
 namespace app\models;
 use \yii\db\ActiveRecord;
 use \yii\web\Identity;
 
+/**
+ * Class User
+ * @package app\models
+ * @property integer $id;
+ * @property string $email
+ * @property string $first_name;
+ * @property string $last_name;
+ * @property string $password;
+ * @property integer $type;
+ */
 class User extends ActiveRecord implements Identity
 {
+    // Constants for user types
+    const TYPE_ADMIN = 0;
+    const TYPE_USER = 1;
+    // Attributes
     public $id;
     public $email;
     public $first_name;
     public $last_name;
     public $password;
     public $type;
-    /**
-     * @return tablename
-     */
+
     public static function tableName()
     {
         return 'users';
+    }
+
+    public function rules()
+    {
+        return array(
+            array('email, password', 'required', 'on' => 'login'),
+            array('email, first_name, last_name, password', 'required', 'on' => 'register')
+        );
+    }
+
+    public function scenarios()
+    {
+        return array(
+            'login' => array('email', 'password'),
+            'register' => array('email','first_name', 'last_name', 'password'),
+        );
     }
 
     public static function findIdentity($id)
@@ -42,8 +64,19 @@ class User extends ActiveRecord implements Identity
     }
 
     public function validateAuthKey($authKey)
-   {
+    {
        // return $this->authKey === $authKey;
-   }
+    }
 
+    public static function addUser($email, $first_name, $last_name, $password, $type = self::TYPE_USER)
+    {
+        $user = new User();
+        $user->scenario ='register';
+        $user->email = $email;
+        $user->first_name = $first_name;
+        $user->last_name = $last_name;
+        $user->password = $password;
+        $user->type = $type;
+        $user->save();
+    }
 }
