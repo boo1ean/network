81550ff5d91ea2986060901122897f66613d97af
Ilya Antipenko <aivus@aivus.name>
2013-07-17 23:47:32 +0300

    Add User tests. (testSettings)

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,59 @@
+<?php
+use Codeception\Util\Stub;
+
+class UserTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \CodeGuy
+    */
+    protected $codeGuy;
+
+    /**
+     * @var \app\models\User
+     */
+    private $user;
+
+    // Default const
+    const EMAIL         = "email@example.com";
+    const PASSWORD      = "TestPassword";
+    const FIRST_NAME    = "User_Firstname";
+    const LAST_NAME     = "User_Lastname";
+
+    protected function _before()
+    {
+        // Create user
+        $this->user = new \app\models\User();
+        $this->user->email = self::EMAIL;
+        $this->user->password = self::PASSWORD;
+        $this->user->first_name = self::FIRST_NAME;
+        $this->user->last_name = self::LAST_NAME;
+        $this->user->save();
+    }
+
+    protected function _after()
+    {
+        // Remove user
+        $this->user->delete();
+    }
+
+    // tests
+    public function testSettings()
+    {
+        // Get empty array by default
+        $setting = $this->user->setting;
+        $this->assertEquals(array(), $setting);
+
+        // Check setter and getter
+        $expected = array("key" => "value", array(array("key" => "value")));
+        $this->user->setting = $expected;
+        $actual = $this->user->setting;
+        $this->assertEquals($expected, $actual);
+
+        // Check User::addSetting method
+        $addArray = array("omg" => "omg value");
+        $this->user->addSetting("omg", "omg value");
+        $this->assertEquals($this->user->setting, array_merge($expected, $addArray));
+
+    }
+
+}
