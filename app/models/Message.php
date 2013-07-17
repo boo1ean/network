bbc418a914ec4ea2346d01c1918bd21a3067bae8
s0ph1e <sonya1912@gmail.com>
2013-07-17 17:21:18 +0300

    Add message model #17

---
+++ app/models/Message.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sophie
+ * Date: 17.07.13
+ * Time: 16:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace app\models;
+use \yii\db\ActiveRecord;
+use app\models\User;
+
+
+class Message extends ActiveRecord
+{
+    /**
+     * @return string name of table in DB
+     */
+    public static function tableName() {
+        return 'messages';
+    }
+
+    /**
+     * @return \yii\db\ActiveRelation object contains author of the message
+     */
+    public function getUser() {
+        return $this->hasOne('user', array('id' => 'user_id'));
+    }
+}
