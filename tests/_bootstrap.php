e835969ea0b9ec2209c5e0348113cc3bebbb5356
boo1ean <boo1ean0807@gmail.com>
2013-07-17 21:04:52 +0300

    Setup Yii app for tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,6 @@
 <?php
-// This is global bootstrap for autoloading 
+require(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
+require(__DIR__ . '/../vendor/autoload.php');
+
+$config = require(__DIR__ . '/../app/config/main.php');
+$application = new yii\web\Application($config);
