309f6d99286a879982fd173fba8cb250192b7df7
kirillstyopkin <kirillstyopkin@gmail.com>
2013-07-21 23:41:17 +0300

    Fix bug active menu item highlight

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -20,7 +20,7 @@ if ($guest) {
     $items[] = array('label' => 'Login', 'url' => array('/auth/login'));
     $items[] = array('label' => 'Registration', 'url' => array('/auth/registration'));
 } else {
-    $items[] = array('label' => 'Messages', 'url' => array('/message/conversation'));
+    $items[] = array('label' => 'Messages', 'url' => array('/message/conversation'), 'active' => Yii::$app->controller->id == 'message');
     $items[] = array('label' => 'Logout', 'url' => array('/auth/logout'));
 }
 
