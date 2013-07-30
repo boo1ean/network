92efc611324e7c03755ed6ad01c55b4c2024681d
adrenalinkin <adrenalinkin@gmail.com>
2013-07-30 22:28:44 +0300

    Regroup  fields in user edit form.

--- app/views/admin/userEdit.php
+++ app/views/admin/userEdit.php
@@ -7,12 +7,18 @@ use yii\helpers\Html;
     $form = ActiveForm::begin(array('options' => array('class' => 'form-horizontal', 'id' => 'edit_user')));
     echo HTML::hiddenInput('id_edit', $model->id_edit, array('id' => 'id_edit'));
 
-    echo $form->field($model, 'email')->textInput(array('value' => $model->email, 'id' => 'email'));
+    echo $form->field($model, 'email')->textInput(array('value'      => $model->email,      'id' => 'email'));
+    echo $form->field($model, 'first_name')->textInput(array('value' => $model->first_name, 'id' => 'first_name'));
+    echo $form->field($model, 'last_name')->textInput(array('value'  => $model->last_name, ' id' => 'last_name'));
+
+    echo HTML::tag('b', 'If you wanna change password for this user, write in both of field below');
     echo $form->field($model, 'password')->passwordInput(array('placeholder' => 'Enter new password', 'id' => 'password'));
     echo $form->field($model, 'repeat_password')->passwordInput(array('placeholder' => 'Repeat password', 'id' => 'repeat_password'));
-    echo $form->field($model, 'first_name')->textInput(array('value' => $model->first_name, 'id' => 'first_name'));
-    echo $form->field($model, 'last_name')->textInput(array('value' => $model->last_name, 'id' => 'last_name'));
 
-    echo Html::button('Confirm', array('class' => 'btn btn-success', 'onclick' => 'return editUser('.$model->id_edit.', 0);'));
+    echo Html::button('Confirm', array(
+        'class'   => 'btn btn-success',
+        'data-id' => $model->id_edit,
+        'onclick' => 'return userEdit(this, 0);'
+    ));
 ?>
 <?php ActiveForm::end(); ?>
