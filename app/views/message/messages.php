d6d5c030da0ddd0445fd121462516092b65dbdcd
s0ph1e <sonya1912@gmail.com>
2013-08-04 11:14:07 +0300

    Rework messages view. Fix #61

--- app/views/message/messages.php
+++ app/views/message/messages.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 ?>
 
-<h1><?php echo $conversationTitle ?> </h1><br>
+<h1><?php echo $conversationTitle ?> </h1>
 <ul class="inline">
     <?php foreach ($conversationMembers as $member):?>
         <li>
@@ -15,52 +15,39 @@ use yii\widgets\ActiveForm;
         <?php echo Html::a('Add user +', 'message/members/' . $conversationId, array('class' => 'btn btn-small btn-primary')); ?>
     </li>
 </ul>
-<br><table id="TableOfMessages">
-    <?php foreach ($messages as $message): ?>
-        <!--<tr>
-            <td id="NamesOfUsersInTableOfMessages">
-                <?php /*echo html::a($message->user->userName, '#', array('class' => 'btn btn-small disabled'));*/?>
-            </td>
-            <td class="MessageCell">
-                <p class="message left">
-                    <?php /*echo $message->body; */?>
-                </p>
-            </td>
-        </tr>-->
-        <div class="messageContainer">
-            <div class = "messageUser">
-                <?php echo html::a($message->user->userName, '#', array('class' => 'btn btn-small disabled'));?>
-            </div>
-            <div class = "messageBody">
-                <div class="popover right in">
-                    <div class="arrow"></div>
-                    <!--<h3 class="popover-title"></h3>-->
-                    <div class="popover-content">
-                        <?php echo $message->body; ?>
-                    </div>
+<!-- Print all messages in conversation -->
+<?php foreach ($messages as $message): ?>
+    <div class = "messageContainer">
+        <div class = "messageUser">
+            <span class = "avatar">  <?php echo Html::img($message->user->avatar); ?> </span>
+        </div>
+        <div class = "messageBody">
+            <div class = "popover right in">
+                <div class = "arrow"></div>
+                <h5 class="popover-title"><?php echo $message->user->userName; ?></h5>
+                <div class = "popover-content">
+                    <?php echo $message->body; ?>
                 </div>
             </div>
         </div>
-    <?php endforeach; ?>
-        <tr>
-            <td></td>
-            <td>
-                <?php
-                $form = ActiveForm::begin(array('options' => array('class' => 'form-inline')));
-                echo Html::textarea('body', '', array(
-                    'placeholder' => 'Write your message here',
-                    'autofocus'   => 'true',
-                    'rows' => '2',
-                    'maxlength' => '100',
-                    'id' => 'MessageForm'));
-                echo Html::submitButton('Send', array(
-                                               'class' => 'btn btn-info',
-                                               'id' => 'MessageSend'));
-                ActiveForm::end();
-                ?>
-            </td>
-        </tr>
-</table>
+    </div>
+<?php endforeach; ?>
+<!-- Input for new message -->
+<div class = "messageContainer">
+    <?php
+    $form = ActiveForm::begin(array('options' => array('class' => 'form-inline')));
+    echo Html::textarea('body', '', array(
+        'placeholder' => 'Write your message here',
+        'autofocus'   => 'true',
+        'rows' => '2',
+        //'maxlength' => '100',
+        'id' => 'MessageForm'));
+    echo Html::submitButton('Send', array(
+        'class' => 'btn btn-info',
+        'id' => 'MessageSend'));
+    ActiveForm::end();
+    ?>
+</div>
 
 
 
