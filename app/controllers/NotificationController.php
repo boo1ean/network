05bcc2b64dcddd9ee539d0520b8762c014e19f9f
s0ph1e <sonya1912@gmail.com>
2013-08-09 11:03:46 +0300

    Implement notifications controller and view. #89

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -1,20 +1,48 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Sophie
- * Date: 09.08.13
- * Time: 9:56
- * To change this template use File | Settings | File Templates.
- */
 
 namespace app\controllers;
 
 use yii\web\Controller;
+use Yii;
 use app\models\User;
 
 class NotificationController extends Controller
 {
+    public function beforeAction($action) {
+        // Check user on access
+        if (Yii::$app->getUser()->getIsGuest()) {
+            Yii::$app->getResponse()->redirect('/');
+            return false;
+        }
+        return true;
+    }
+
     public function actionIndex(){
-        return $this->render('all');
+        /**
+         * @var User $user current user
+         */
+        $user = Yii::$app->getUser()->getIdentity();
+        $viewParameters = array();
+        // Get all user's notifications
+        $notifications = $user->notifications;
+        foreach ($notifications as $notification) {
+            // Current notification data
+            $row = array();
+            // Get type of notification
+            $class = get_class($notification);
+            switch ($class) {
+                case 'app\models\Conversation':
+                    $row['icon'] = 'icon-envelope';
+                    $row['link'] = '/message/conversation/' . $notification->id;
+                    $row['title'] = $notification->title;
+                    $row['description'] = 'Last message was sent on ' . $notification->lastMessageTime;
+                    break;
+            }
+            $viewParameters[] = $row;
+        }
+        //var_dump($viewParameters); die();
+        return $this->render('all', array(
+            'notifications' => $viewParameters,
+        ));
     }
 }
--- app/views/notification/all.php
+++ app/views/notification/all.php
@@ -2,3 +2,16 @@
 use yii\helpers\Html;
 ?>
 <h1>Notifications</h1>
+<ul class="nav nav-stacked" id = "notification-list">
+    <?php foreach ($notifications as $notification): ?>
+    <li class = "notification">
+        <?php
+        echo Html::beginTag('a', array('href'  => $notification['link']));
+        echo Html::tag('i', '', array('class' => $notification['icon']));
+        echo Html::tag('span', $notification['title']);
+        echo Html::tag('p', $notification['description']);
+        echo Html::endTag('a');
+        ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
