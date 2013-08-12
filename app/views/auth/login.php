1663ab475d823455f480649bc4991401afbd6c94
s0ph1e <sonya1912@gmail.com>
2013-08-12 17:59:31 +0300

    Fix password input in login view

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -14,7 +14,7 @@ use yii\widgets\Captcha;
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <?echo $form->field($model, 'password')->passwordInput();?>
+                <?php echo $form->field($model, 'password')->passwordInput();?>
             </div>
             <?php //echo $form->field($model, 'captcha')->widget(Captcha::className(), array('options' => array('class' => 'input-small'), 'template' => "{input}<br>{image}", 'captchaAction' => 'auth/captcha'));?>
         </div>
