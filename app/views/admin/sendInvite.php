2f6b2c187787eb957750c2e25485e6254bc4fd1e
adrenalinkin <adrenalinkin@gmail.com>
2013-08-19 01:06:12 +0300

    Update style for users list and invitation from.

--- app/views/admin/sendInvite.php
+++ app/views/admin/sendInvite.php
@@ -2,18 +2,21 @@
     use yii\widgets\ActiveForm;
     use yii\helpers\Html;
 ?>
+<div class="col-lg-offset-2">
+    <h1>Send invite</h1>
 
-<h1>Send invite</h1>
+    <?php $form = ActiveForm::begin();?>
+    <div class="row">
+        <div class="col-lg-6">
+            <?php if(isset($message) && is_string($message)) {
+                echo Html::tag('div', $message, array('class' => "alert alert-success"));
+                echo Html::a('back', '/admin', array('class' => 'btn btn-success'));
+            } else {
+                echo $form->field($model, 'email')->textInput();
+                echo Html::submitButton('Send', array('class' => 'btn btn-success'));
+            }?>
+        </div>
+    </div>
 
-<?php
-    $form = ActiveForm::begin();
-    if(isset($message) && is_string($message)) {
-        echo Html::tag('div', $message, array('class' => "alert alert-success"));
-        echo Html::a('back', '/admin', array('class' => 'btn btn-success'));
-    } else {
-        echo $form->field($model, 'email')->textInput();
-        echo Html::submitButton('Send', array('class' => 'btn btn-success'));
-    }
-?>
-
-<?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
+</div>
--- app/views/admin/userList.php
+++ app/views/admin/userList.php
@@ -2,50 +2,51 @@
     use yii\helpers\Html;
     use yii\widgets\LinkPager;
 ?>
-
+<div class="col-lg-offset-1">
     <h1>Users list</h1>
-<div class="modal fade" id="user-modal"></div>
-<table class="table table-hover">
-    <tr >
-        <td > <?php echo Html::tag('p', 'Avatar'); ?>    </td>
-        <td > <?php echo Html::tag('p', 'E-mail');?>     </td>
-        <td > <?php echo Html::tag('p', 'First name');?> </td>
-        <td > <?php echo Html::tag('p', 'Last name');?>  </td>
-        <td > <?php echo Html::tag('p', 'Actions');?>    </td>
-    </tr>
-    <?php foreach($users as $user):?>
-        <tr <?php echo $user->is_active ? '' : 'class="danger"' ?> >
-            <td id="<?php echo $user->id?>_avatar" class="text-center">
-                <?php echo Html::img($user->avatar, array('class' => 'avatar small')); ?>
-            </td>
-            <td id="<?php echo $user->id?>_email">      <?php echo $user->email;?>              </td>
-            <td id="<?php echo $user->id?>_first_name"> <?php echo $user->first_name;?>         </td>
-            <td id="<?php echo $user->id?>_last_name">  <?php echo $user->last_name;?>          </td>
-            <td >
-            <?php
-
-                echo Html::button('Edit', array(
-                    'class'       => 'btn btn-sm btn-success',
-                    'data-id'     => $user->id,
-                    'name'        => 'user-edit'
-                ));
-                echo Html::submitButton($user->is_active ? 'Block account' : 'Unblock account', array(
-                    'class'   => $user->is_active ? 'btn btn-sm btn-warning' : 'btn btn-sm btn-info',
-                    'data-id' => $user->id,
-                    'name'    => 'user-block'
-                ));
-                echo Html::submitButton('Delete', array(
-                    'class'   => 'btn btn-sm btn-danger',
-                    'data-id' => $user->id,
-                    'name'    => 'user-delete'
-                ));
-            ?>
-            </td>
+    <div class="modal fade" id="user-modal"></div>
+    <table class="table table-hover">
+        <tr >
+            <td > <?php echo Html::tag('p', 'Avatar'); ?>    </td>
+            <td > <?php echo Html::tag('p', 'E-mail');?>     </td>
+            <td > <?php echo Html::tag('p', 'First name');?> </td>
+            <td > <?php echo Html::tag('p', 'Last name');?>  </td>
+            <td > <?php echo Html::tag('p', 'Actions');?>    </td>
         </tr>
-    <?php endforeach;?>
-</table>
-<?php if(!is_null($pagination)):?>
-    <div class="pagination-centered">
-        <?php echo LinkPager::widget(array('pagination' => $pagination)); ?>
-    </div>
-<?php endif;?>
+        <?php foreach($users as $user):?>
+            <tr <?php echo $user->is_active ? '' : 'class="danger"' ?> >
+                <td id="<?php echo $user->id?>_avatar" class="text-center">
+                    <?php echo Html::img($user->avatar, array('class' => 'avatar small')); ?>
+                </td>
+                <td id="<?php echo $user->id?>_email">      <?php echo $user->email;?>              </td>
+                <td id="<?php echo $user->id?>_first_name"> <?php echo $user->first_name;?>         </td>
+                <td id="<?php echo $user->id?>_last_name">  <?php echo $user->last_name;?>          </td>
+                <td >
+                <?php
+
+                    echo Html::button('Edit', array(
+                        'class'   => 'btn btn-sm btn-success',
+                        'data-id' => $user->id,
+                        'name'    => 'user-edit'
+                    ));
+                    echo Html::submitButton($user->is_active ? 'Block account' : 'Unblock account', array(
+                        'class'   => $user->is_active ? 'btn btn-sm btn-warning' : 'btn btn-sm btn-info',
+                        'data-id' => $user->id,
+                        'name'    => 'user-block'
+                    ));
+                    echo Html::submitButton('Delete', array(
+                        'class'   => 'btn btn-sm btn-danger',
+                        'data-id' => $user->id,
+                        'name'    => 'user-delete'
+                    ));
+                ?>
+                </td>
+            </tr>
+        <?php endforeach;?>
+    </table>
+    <?php if(!is_null($pagination)):?>
+        <div class="pagination-centered">
+            <?php echo LinkPager::widget(array('pagination' => $pagination)); ?>
+        </div>
+    <?php endif;?>
+</div>
