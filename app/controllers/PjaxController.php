bfe27d0fb600d3a848821e45fea7203a3177f007
s0ph1e <sonya1912@gmail.com>
2013-08-20 14:15:44 +0300

    Set last user activity on each action. #87

--- app/controllers/PjaxController.php
+++ app/controllers/PjaxController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use yii;
 
 class PjaxController extends Controller
 {
@@ -11,6 +12,14 @@ class PjaxController extends Controller
             $this->layout = 'pjax';
         }
         return parent::render($view, $data);
+    }
+
+    public function beforeAction($action) {
+        $user = Yii::$app->getUser()->getIdentity();
+        $user->last_activity = date('Y-m-d H:i:s');
+        $user->update();
+        
+        return parent::beforeAction($action);
     }
 
 } 
