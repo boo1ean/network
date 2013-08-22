f4d77faee49480bf7171595c3333aca227d15ce0
adrenalinkin <adrenalinkin@gmail.com>
2013-08-22 23:50:06 +0300

    Compact books view #109.

--- app/views/library/bookList.php
+++ app/views/library/bookList.php
@@ -6,47 +6,49 @@ use app\models\Book;
 
 <div class="col-lg-offset-1">
     <h1> Library </h1>
-
-    <div class="bookslist">
+    <ul class="nav nav-list panel-group" id="books-list">
         <?php foreach ($books as $book):?>
-            <ul class="nav nav-list">
-                <li>
-                    <p><?php echo $book['author']; ?></p>
-                    <p class='lead'>
-                        <?php echo $book['title']; ?>
-                        <?php if ($book['status'] == 'available'): ?>
-                        <span class='label label-success'><?php echo $book['status']; ?></span></p>
-                    <?php else: ?>
-                        <span class='label label-danger'><?php echo $book['status']; ?></span></p>
-                        <br/><br/>
-                    <?php endif; ?>
+            <li <?php echo $book->status == 'available' ? 'class="panel panel-success"' : 'class="panel panel-danger"'?>>
+                <div class="panel-heading ">
+                    <h4 class="panel-title modal-header">
+                        <div class="navbar-left">
+                            <b class="h2" title="Book title">
+                                <?php echo $book->title; ?>
+                            </b>
+                            <div title="Book author">
+                                <?php echo $book->author; ?>
+                            </div>
+                        </div>
+                        <div class="navbar-right">
+                            <?php
+                            $book_current = Book::find($book->id);
+                            $tags_by_book = $book_current->tags;
 
-                    <blockquote>
-                        <p><?php echo $book->description; ?></p>
-                    </blockquote>
-
+                            foreach ($tags_by_book as $tag) {
+                                echo Html::a($tag->title, null, array(
+                                    'id'      => $tag->title,
+                                    'class'   => 'label label-info'
+                                )).' ';
+                            } ?>
+                        </div> <br/><br/><br/><br/>
+                        <a class="accordion-toggle" data-parent="#books-list" data-toggle="collapse" href="<?php echo '#'.$book->id.'-collapse'?>">
+                            Description
+                        </a>
+                    </h4>
                     <?php if ($book->type == 2): ?>
                         <span class='label label-success'>
                         <?php echo Html::a('Download Ebook', $book->link, array('target' => '_blank')); ?>
                     </span>
                     <?php endif;?>
-                </li>
-            </ul>
-
-            <h4 class="tag">
-                <?php
-                    $book_current = Book::find($book->id);
-                    $tags_by_book = $book_current->tags;
-
-                    foreach ($tags_by_book as $tag) {
-                        echo Html::a($tag->title, null, array(
-                                'id' => $tag->title,
-                                'onclick' => 'return showByTags(this)',
-                                'class' => 'label label-info'
-                            )).' ';
-                    } ?>
-            </h4>
-
+                </div>
+                <div id="<?php echo $book->id.'-collapse'?>" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <blockquote>
+                            <p><?php echo $book->description; ?></p>
+                        </blockquote>
+                    </div>
+                </div>
+            </li>
         <?php endforeach;?>
-    </div>
+    </ul>
 </div>
