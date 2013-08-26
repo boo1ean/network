d9242850022cf2969de0f59bd15043e00d8c3d85
s0ph1e <sonya1912@gmail.com>
2013-08-26 18:42:43 +0300

    Fix margin in notifications view.

--- app/views/notification/all.php
+++ app/views/notification/all.php
@@ -1,19 +1,18 @@
 <?php
 use yii\helpers\Html;
 ?>
-<div class="col-lg-offset-2">
-    <h1>Notifications</h1>
-    <ul class="nav nav-stacked" id = "notification-list">
-        <?php foreach ($notifications as $notification): ?>
-        <li class = "notification">
-            <?php
-            echo Html::beginTag('a', array('href'  => $notification['link']));
-            echo Html::tag('span', '', array('class' => $notification['icon']));
-            echo Html::tag('span', $notification['title']);
-            echo Html::tag('p', $notification['description']);
-            echo Html::endTag('a');
-            ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-</div>
+
+<h1>Notifications</h1>
+<ul class="nav nav-stacked" id = "notification-list">
+    <?php foreach ($notifications as $notification): ?>
+    <li class = "notification">
+        <?php
+        echo Html::beginTag('a', array('href'  => $notification['link']));
+        echo Html::tag('span', '', array('class' => $notification['icon']));
+        echo Html::tag('span', $notification['title']);
+        echo Html::tag('p', $notification['description']);
+        echo Html::endTag('a');
+        ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
